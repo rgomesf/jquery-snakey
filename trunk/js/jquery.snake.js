c5e4ca134dabc8b1bce354850cb41d02e2491783
willis.rh <willis.rh@04bb2c66-8ff8-11dd-88a1-c356c4ec900a>
2008-12-02 21:32:17 +0000

    properly implemented snake speed... speed is also now set as a level attribute

--- trunk/js/jquery.snake.js
+++ trunk/js/jquery.snake.js
@@ -51,6 +51,9 @@ var Snake = {
 	},
 		
 	start : function() {
+		// set initial speed
+		Snake.speed = Level[Snake.level][0].speed;
+		// show the cherry, and start the animation
 		Snake.$cherry.fadeIn(function(){
 			Snake.animateTimer = setInterval(Snake.animate, Snake.speed);			
 		});
@@ -87,41 +90,44 @@ var Snake = {
 		Snake.$cherry.hide();
 
 		// update map message	
-		$("#map-msg").hide().html("Level "+Snake.level+"<small><br/>Eat <strong>"+Level[Snake.level][0].cherries+"</strong> cherries</small>").fadeIn();
+		$("#map-msg").hide().html('\
+			Level '+Snake.level+'<br/>\
+			<small>Eat <strong>'+Level[Snake.level][0].cherries+'</strong> cherries<small><br/>\
+			<small style="font-size:80%"><strong>('+Snake.lives+'</strong> '+(Snake.lives>1?'lives':'life')+' remaining)</small></small>\
+		').fadeIn(500, function(){
 
-		setTimeout(function(){
+			setTimeout(function(){
+	
+				// hide map message
+				$("#map-msg").fadeOut(500, function(){
 
-			// hide map message
-			$("#map-msg").fadeOut(500, function(){
+					// hide overlay
+					Snake.$overlay.hide();
 
-				// hide overlay
-				Snake.$overlay.hide();
+					// reset and generate wall
+					Snake.wallseg = {};
+					Snake.Wall.generate();
+					
+					// generate cherry
+					Snake.Cherry.generate(false);
 
-				// reset and generate wall
-				Snake.wallseg = {};
-				Snake.Wall.generate();
+					// reset, remove and re-append snake segments to map
+					Snake.seg = {length:Level[Snake.level][0].length};
+					for(var i=0;i<Snake.seg.length;i++) {
+						Snake.seg[i] = $('<span class="snake '+i+'"></span>').appendTo(Snake.$map);
+						Snake.seg[i].top = Snake.seg[i].left = 0;
+					}
 				
-				// hide then reposition cherry
-				Snake.Cherry.generate(false);
-
-				// reset, remove and re-append snake segments to map
-				Snake.seg = {length:Level[Snake.level][0].length};
-				for(var i=0;i<Snake.seg.length;i++) {
-					Snake.seg[i] = $('<span class="snake '+i+'"></span>').appendTo(Snake.$map);
-					Snake.seg[i].top = Snake.seg[i].left = 0;
-				}
-				$(".snake").show();
-				
-				// start animation
-				setTimeout(function(){
-					// reset direction and speed
-					Snake.cache.keyCode[0] = 0;
-					Snake.cache.keyCode[1] = 39;
-					Snake.speed = 100;
-					Snake.start();
-				}, 1000);
-			});
-		}, 1800);
+					// start animation
+					setTimeout(function(){
+						// reset direction
+						Snake.cache.keyCode[0] = 0;
+						Snake.cache.keyCode[1] = 39;
+						Snake.start();
+					}, 1000);
+				});
+			}, 2500);
+		});
 	},
 			
 	animate : function() {			
@@ -229,6 +235,7 @@ var Snake = {
 		// adjust speed
 		Snake.speed -= 1;
 		$("#stats-speed").html(Snake.speed);
+
 		clearInterval(Snake.animateTimer);
 		Snake.animateTimer = setInterval(Snake.animate, Snake.speed);			
 		return false;
@@ -239,6 +246,7 @@ var Snake = {
 			Snake.finishedGame();	
 		} else {
 			Snake.level++;
+			Snake.speed = Level[Snake.level][0].speed;
 			Snake.newGame();
 		}
 	},
@@ -274,13 +282,13 @@ var Snake = {
 			Snake.newGame();
 		} else {
 			Snake.pause();
-			$("#map-msg").html('You Died<small><br/><a href="javascript:;" onclick="Snake.newGame(true)">Play again?</a></small>');
+			$("#map-msg").html('<br/>You Died<small><br/><a href="javascript:;" onclick="Snake.newGame(true)">Play again?</a></small>');
 		}
 	},
 
 	finishedGame : function(){
 		Snake.pause();
-		$("#map-msg").html('Well Done! You finished.<small><br/><a href="javascript:;" onclick="Snake.newGame(true)">Play again?</a></small>');
+		$("#map-msg").html('<br/>Well Done! You finished.<small><br/><a href="javascript:;" onclick="Snake.newGame(true)">Play again?</a></small>');
 	},
 
 	Cherry : {	
@@ -339,17 +347,17 @@ var Snake = {
 Level = [
 	,
 	[
-		{cherries : 5, length : 10},
+		{cherries : 5, length : 10, speed : 100},
 		{seg : 30, top : 50, left : 200}
 	],
 	[
-		{cherries : 5, length : 15},
+		{cherries : 5, length : 15, speed : 95},
 		{seg : 30, top : 50, left : 100},
 		{seg : 30, top : 50, left : 300}
 	],
 	[
 				
-		{cherries : 5, length : 20},
+		{cherries : 5, length : 20, speed : 90},
 		{seg : 12, top : 10, left : 50},
 		{seg : 12, top : 0, left : 350},
 		{seg : 12, top : 270, left : 350},
@@ -357,14 +365,14 @@ Level = [
 		{seg : 24, top : 80, left : 200}
 	],
 	[
-		{cherries : 5, length : 25},
+		{cherries : 5, length : 25, speed : 85},
 		{seg : 19, top : 0, left : 200},
 		{seg : 20, top : 200, left : 200},
 		{seg : 40, top : 0, left : 0},
 		{seg : 40, top : 0, left : 390}
 	],
 	[
-		{cherries : 5, length : 30},
+		{cherries : 5, length : 30, speed : 80},
 		{seg : 20, top : 10, left : 50},
 		{seg : 20, top : 0, left : 150},
 		{seg : 20, top : 10, left : 250},
@@ -375,7 +383,7 @@ Level = [
 		{seg : 20, top : 200, left : 320}
 	],
 	[
-		{cherries : 5, length : 35},
+		{cherries : 5, length : 35, speed : 75},
 		{seg : 2, top : 10, left : 200},
 		{seg : 2, top : 40, left : 200},
 		{seg : 2, top : 70, left : 200},

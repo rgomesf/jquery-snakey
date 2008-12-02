ecbc39f5131148caadb5568563d7e3020283a41f
willis.rh <willis.rh@04bb2c66-8ff8-11dd-88a1-c356c4ec900a>
2008-12-02 00:31:33 +0000

    small update to html

--- trunk/js/common.js
+++ trunk/js/common.js
@@ -7,26 +7,63 @@
 
 $(function(){
 	// FF2/Mac Opacity Bug
-	($.browser.mozilla && parseFloat($.browser.version) < 1.9 && navigator.appVersion.indexOf('Mac') !== -1) && $('body').css('-moz-opacity',.999);
-			
+	($.browser.mozilla && parseFloat($.browser.version) < 1.9 && 
+	navigator.appVersion.indexOf('Mac') !== -1) && 
+	$('body').css('-moz-opacity',.999);
+	
+	// IE6 background css flickering bug
+	(!$.browser.msie) && (function(){
+		try{document.execCommand('BackgroundImageCache', false, true);}
+		catch(e){};
+	})();
+	
 	// setup the game
 	Snake.setup();
 			
+	// start the game
 	$("a#start-game").click(function(e){
 		e.preventDefault();
-		// start the game
-		Snake.newGame();
+		Snake.newGame(true);
 	});
+
+	$("a.accordian:not('a[href=#high-scores])").each(function(){
+		$(this).click(function(e){
+			e.preventDefault();
+			$("#"+this.href.replace(/[^#]+#/, '')).animate({height:"toggle",opacity:"toggle"}, 560, "jswing");
+		});
+	});
+
+	var timer = 0;
 			
-	$("a#bugs-exp").click(function(){
-		$(".bugs").slideToggle();
-	});
-	$("a#source-exp").click(function(){
-		$(".source").slideToggle();
-	});
-	$("a#scores-exp").click(function(){
+	// get the high scores
+	$("a[href='#high-scores']").click(function(e){
+
+		// until i've completed scoring, i'll just display a 'coming soon' message
+		e.preventDefault();
+		var
+			anchor = this,
+			$msg =	!
+				$(this).next().length 
+				? 
+					$('<small><em>&nbsp;(coming soon)</em></small>')
+					.css({display:"none",color:"#000"})
+					.attr("id", "comingsoon") 
+				: 
+				$("#comingsoon");
+
+		!$(this).next().length && (function(){
+			$(anchor).after($msg);
+		})();
+		if ($msg[0].style.display == 'none') {
+			timer = setTimeout(function(){
+				$msg.fadeOut();
+			}, 3000);
+			$msg.fadeIn();
+		} else {
+			clearTimeout(timer);
+			$msg.show().fadeOut();
+		}
 		return;
-		// get the high scores
 		$.ajax({
 			type : "GET",
 			url : "snakey-scores.php",
@@ -38,13 +75,21 @@ $(function(){
 							<span class=\"col1\">Pos.</span>\
 							<span class=\"col2\">Score</span>\
 							<span class=\"col3\">Name</span>&nbsp;</span>");
+				var $scores = $("<span class=\"scroller\"></span>");
 				for(var i in data) {
-                      	        	 $("#high-scores").append("<span class=\"row\">\
+                      	        	 $scores.append("<span class=\"row\">\
 								<span class=\"col1\">"+(parseInt(i)+1)+"</span>\
 								<span class=\"col2\">"+data[i][0]+"</span>\
 								<span class=\"col3\">"+data[i][1]+"</span>&nbsp;</span>");
 				}
-	                	$("#high-scores").slideToggle();
+	                	$("#high-scores").append($scores).slideToggle();
+				$("#high-scores .row").hover(
+				function(){
+					this.style.background="#eee";
+				},
+				function(){
+					this.style.background="#fff";
+				});
        	                }
 		});
 	});
